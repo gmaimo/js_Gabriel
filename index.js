72ba10bc8ace7fa0e69c0ec915f782dfd5a5e6d0
GabrielMaimoRigo <bielmaimo16@gmail.com>
2022-04-26 19:07:23 +0200

    js ejercicios 1-5

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("¡Empezamos con javascript!");
+
+console.log("Hola, soy un estudiante Hackademy");
+
+let ingredientes = "Mozzarella, tomate";
+
+console.log(ingredientes);
+
+ingredientes = "Mozzarella, tomate, albahaca";
+
+console.log(ingredientes);
+
+let inicioFrase = "Tres tristes tigres ";
+
+const seguirFrase = "tragan trigo en un trigal";
+
+console.log(inicioFrase + seguirFrase);
