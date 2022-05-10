699d2f7b97c5b1a64a00e0513979acc551b88e90
GabrielMaimoRigo <bielmaimo16@gmail.com>
2022-05-10 21:21:53 +0200

    to 37

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ if (num > 100){
     alert("es menor de 100") */
 
 //ej. 16
-/* let n =  prompt("Escribe un numero y te diré si es positivo o negativo");
+/* let n = prompt("Escribe un numero y te diré si es positivo o negativo");
 
 if (n >= 0){
     alert("es positivo")
@@ -173,6 +173,7 @@ if (buen_tiempo && !trabajo){
     console.log(`${x-5}`)
 }
  */
+
 //ej. 21
 /* let b = 0;
 
@@ -191,7 +192,7 @@ for(b=0; b<=i; b++){
 alert(`La suma de los primeros ${i} números da: ${a}`);  */
 
 //ej. 23
-let n = Number(prompt("inseta un número y te diré su factorial"));
+/* let n = Number(prompt("inseta un número y te diré su factorial"));
 let a = 1;
 
 if (typeof n == "number" && !Number.isNaN(n) && Number.isInteger(n)){
@@ -202,13 +203,26 @@ if (typeof n == "number" && !Number.isNaN(n) && Number.isInteger(n)){
 
 if(typeof n == "number" && !Number.isNaN(n) && Number.isInteger(n)){
     alert(`el factorial de ${n} es: ${a}`);
+} */
+
+//ej. 24
+/* let min = 1;
+let max = 100;
+
+console.log(Math.round(Math.random() * (max - min) + min)); */
+
+//ej. 25
+/* 
+function tipCalculator(customerService, totalPrice){
+    if (customerService == "fatal") return ((totalPrice * 0.05) + totalPrice);
+    if (customerService == "ok") return ((totalPrice * 0.15) + totalPrice);
+    if (customerService == "bueno") return ((totalPrice * 0.2) + totalPrice);
+    if (customerService == "excelente") return ((totalPrice * 0.3) + totalPrice);
+    return ((totalPrice * 0.18) + totalPrice);
 }
 
-
-
-
-
-
+console.log((tipCalculator(0,10)).toFixed(2));
+ */
 
 //ej. 26
 /* for (let a = 1; a <= 100; a++){
@@ -218,5 +232,138 @@ if(typeof n == "number" && !Number.isNaN(n) && Number.isInteger(n)){
         console.log(`${a}: script`)
     }else if(a%3 == 0){
         console.log(`${a}: java`);
-    }
+    }else console.log(`${a}`);
 } */
+
+//ej. 27
+
+/* function isPar(number){
+    if (number%2 == 0) return (`${number} is even`)
+    return (`${number} is odd`)
+}
+
+console.log(isPar(5)); */
+
+//ej. 28
+
+/* function interval(a, b){
+    for(let i = a; i<= b; i+=2){
+        if(i%2 == 0) i++;
+        console.log(i);
+    }
+}
+
+interval(0, 10); */
+
+//ej. 29
+
+/* function calc (width, length){
+    if (width == length) return Math.pow(width,2);
+    else return width * 2 + length * 2
+}
+
+console.log(calc(20,20)); */
+
+//ej. 30
+
+/* function areaCirculo(){
+    for (let radio = 1; radio <= 10; radio++){
+        console.log((Math.PI * Math.pow(radio,2)).toFixed(3));
+    }
+}
+
+areaCirculo();
+
+function thisArea(radio){
+    console.log((Math.PI * Math.pow(radio,2)).toFixed(3));
+}
+
+thisArea(8); */
+
+// ej. 31
+
+/* function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+let note = "hacer todos los ejercicios";
+
+console.log(capitalize(note)); */
+
+//ej. 32
+
+/* let fib = [0,1];
+let n = 100; // Hasta donde calculamos Fibonacci
+
+for (let i = 2; i < n; i++){
+    fib[i] = fib[i-1] + fib[i-2];
+}
+
+console.log(fib); */
+
+//ej. 33
+
+/* const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+const pares = numeros.filter(numeros => numeros%2 == 0)
+
+console.log(pares); */
+
+/* const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numeros.length; i++){
+    if(i%2 == 0){
+        numeros[i] = numeros[i+1];
+    }
+}
+
+const numerosPares = new Set(numeros);
+
+console.log(numerosPares); */
+
+//ej. 34
+
+/* const días = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes',
+'sabado', 'domingo'];
+
+//console.log(días[0] == "lunes")
+
+const díasM = días.filter(elem => {return elem.charAt(0) == "m"})
+const díasSinM = días.filter(días => días[0] != "m");
+
+console.log(díasM);
+console.log(díasSinM); */
+
+//ej. 35
+
+/* let num = [100, 2, 20, 35, 4, 44];
+
+num.sort((a,b)=>a-b);
+
+const numMayores = num.filter(num => num>10)
+
+console.log(numMayores); */
+
+//ej. 36
+
+/* function dosEdadesMasAltas(array){
+    array.sort((a,b)=>a-b);
+    return array.slice(array.length-2)
+}
+
+console.log(dosEdadesMasAltas([1, 2, 10, 8, 22, 3, 6, 12, 80, 4, 55, 19])) // [80, 55]
+ */
+
+//ej. 37
+
+/* const array = [2,4,25,2,2,4,2,40,23,5];
+
+const arrayPot3 = [];
+
+for (let i = 0; i < array.length; i++){
+    arrayPot3[i] = Math.pow(array[i],3);
+}
+
+console.log(arrayPot3); */
+
+//ej. 38
